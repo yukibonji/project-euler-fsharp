6ba2604a0db6eb24c9325170f4df1f5170727be3
kerams <nphx@outlook.com>
2014-12-04 18:47:31 +0100

    Problem 1

---
+++ project-euler-fsharp/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let problem1a =
+    seq { for n in 1 .. 999 do if n % 3 = 0 || n % 5 = 0 then yield n } |> Seq.sum
+
+let problem1b =
+    seq { for n in 1 .. 999 do if n % 3 = 0 || n % 5 = 0 then yield n } |> Seq.fold (fun running next -> running + next) 0
+
+[<EntryPoint>]
+let main argv = 
+    problem1a |> printfn "%d"
+    0
