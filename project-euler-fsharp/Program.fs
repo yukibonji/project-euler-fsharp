9bd7faa0d4177837711835f910703e71b21dd4a7
kerams <nphx@outlook.com>
2014-12-04 22:18:44 +0100

    Problem 3

--- project-euler-fsharp/Program.fs
+++ project-euler-fsharp/Program.fs
@@ -19,9 +19,41 @@ let problem2 () =
 
     ref 0 |> fibEvenSum 1 1 |> (!)
 
+let problem3a () =
+    let factorsOf (n: int64) =
+       let root = n |> float |> sqrt |> int64
+       seq { for x in 2L .. root do if n % x = 0L then yield x }
+
+    let isPrime n =
+        factorsOf n |> Seq.length = 0
+
+    let input = 600851475143L
+    let factorsOfInput = factorsOf input
+
+    if factorsOfInput |> Seq.length = 0 then
+        input
+    else
+        factorsOfInput |> Seq.filter isPrime |> Seq.max
+
+let problem3b () =
+    let input = 600851475143L
+
+    let mutable n = 2L
+    let mutable roof = input
+    let mutable largestFactor = 1L
+
+    while (n <= roof) do
+        if roof % n = 0L then
+            roof <- roof / n
+            largestFactor <- n
+        else
+            n <- n + 1L
+
+    largestFactor
+
 [<EntryPoint>]
 let main argv = 
-    problem2 () |> printfn "%d"
+    problem3b () |> printfn "%d"
 
     Console.ReadKey(true) |> ignore
     0
