cf3d85dcb88ee3c3027d9e4170171936b9cb7cef
kerams <nphx@outlook.com>
2014-12-05 23:22:27 +0100

    Problem 5

--- project-euler-fsharp/Program.fs
+++ project-euler-fsharp/Program.fs
@@ -57,9 +57,19 @@ let problem4 () =
     |> Seq.map (fun (i, j) -> i * j)
     |> Seq.max
 
+let problem5 () =
+    let divisors = [ 2L; 3L; 4L; 5L; 6L; 7L; 8L; 9L; 11L; 13L; 16L; 17L; 19L ]
+
+    let rec gcd a b =
+        match a % b with
+        | 0L -> b
+        | n -> gcd b n
+
+    divisors |> Seq.fold (fun cur next -> (*) cur next |> (/) <| gcd cur next) 1L
+    
 [<EntryPoint>]
 let main argv = 
-    problem4() |> printfn "%d"
+    problem5() |> printfn "%d"
 
     Console.ReadKey(true) |> ignore
     0
