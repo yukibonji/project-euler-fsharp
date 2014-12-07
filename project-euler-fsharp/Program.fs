f5b5cf6aebaa9f10679a8bd24fb4411587fafa51
kerams <nphx@outlook.com>
2014-12-07 15:06:36 +0100

    Problem 13

--- project-euler-fsharp/Program.fs
+++ project-euler-fsharp/Program.fs
@@ -15,7 +15,7 @@ let time f =
 
 [<EntryPoint>]
 let main argv = 
-    let i, j = time Problems11to20.problem12
+    let i, j = time Problems11to20.problem13
     printfn "%d, %dms" i <| int j
     Console.ReadKey(true) |> ignore
     0
