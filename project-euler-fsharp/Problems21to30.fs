93599817992986a30880cfa8be3aaeb2a8110fc4
kerams <nphx@outlook.com>
2014-12-09 17:25:49 +0100

    Problem 22

--- project-euler-fsharp/Problems21to30.fs
+++ project-euler-fsharp/Problems21to30.fs
@@ -16,3 +16,12 @@ let problem21 () =
     seq { 0 .. 9999 }
     |> Seq.filter isAmicable
     |> Seq.sum
+
+let problem22 () =
+    let raw = readLines 22 |> Seq.nth 0
+
+    raw.Split(',')
+    |> Seq.map (fun s -> s.Substring(1, s.Length - 2))
+    |> Seq.sort
+    |> Seq.fold (fun (sum, i) s -> s |> Seq.sumBy (fun c -> int c - 64) |> (*) i |> (+) sum, i + 1) (0, 1)
+    |> fst
--- project-euler-fsharp/Program.fs
+++ project-euler-fsharp/Program.fs
@@ -15,7 +15,7 @@ let time f =
 
 [<EntryPoint>]
 let main argv = 
-    let i, j = time Problems21to30.problem21
+    let i, j = time Problems21to30.problem22
     printfn "%d, %.3fms" i <| j
     Console.ReadKey(true) |> ignore
     0
